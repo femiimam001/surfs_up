299b3d5f61b8d4a3a79c444c37e2dc3c3b9146c4
femiimam001 <olufemi.imam@gmail.com>
2022-03-23 21:39:48 -0400

    adding new files

---
+++ app.py
@@ -0,0 +1,16 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
